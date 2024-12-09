c92bdaca64e5685df4863bae38be8429a48459f3
Garrett Rowe <garrett.m.rowe@gmail.com>
2024-12-09 09:47:13 -0700

    Update build.sh to be usable on Mac

--- build.sh
+++ build.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
-SRC="main.c"
-OUT="sorta"
-CFLAGS="-Wall -Wextra -Iraylib/build/raylib/include -Lraylib/build/raylib -lraylib -lm"
+# Detect the operating system
+OS=$(uname)
 
-# Build the executable
-clang $SRC -o $OUT $CFLAGS
+# Common flags
+SRC="main.c"
+OUTPUT="sorta"
+INCLUDE="-Iraylib/src"
+CFLAGS="-Wall -Wextra"
+
+if [ "$OS" = "Darwin" ]; then
+    # macOS-specific options
+    LIB_PATH="-Lraylib/build/raylib"
+    LIBS="-lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo"
+elif [ "$OS" = "Linux" ]; then
+    # Linux-specific options
+    LIB_PATH="-Lraylib/build/raylib"
+    LIBS="-lraylib -lm"
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
+
+# Compile
+clang -o $OUTPUT $SRC $INCLUDE $LIB_PATH $LIBS $CFLAGS
 
 # Update the etags files
-etags --language=c *.c  raylib/src/*.c raylib/src/*.h
+# etags --language=c *.c  raylib/src/*.c raylib/src/*.h
+
+
